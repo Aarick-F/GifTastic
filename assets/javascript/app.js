5aa5fa315064eaaa4c1dd8c08f99646de03a2584
Aarick-F <zeflyndesign@gmail.com>
2018-03-18 19:03:14 -0400

    setup preliminary ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
 $(document).ready(() => {
-  
+  // GIPHY API KEY
+  const topics = ["Anger", "Anticipation", "Joy", "Trust",
+                  "Sadness", "Fear", "Surprise", "Disgust"];
+  const myKey = "Ab7efgCTM4wUS3UD0wzdSdgIYDyj8H0q";
+
+  const query = "http://api.giphy.com/v1/gifs/search?q="
+                + topics[0] + "&api_key=" + myKey + "&limit=10";
+  $.ajax({
+    url: query,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response.data);
+  });
+
 });
