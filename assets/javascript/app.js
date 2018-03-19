bec8c33b71d55998302b7e01d69c59467d4347a2
Aarick-F <zeflyndesign@gmail.com>
2018-03-19 19:33:50 -0400

    added styling and initial js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,39 @@
 $(document).ready(() => {
+  
+  let pickedTopic;
+  const topics = ["Anger", "Surprise", "Joy", "Trust",
+                  "Sadness", "Fear", "Disgust"];
   // GIPHY API KEY
-  const topics = ["Anger", "Anticipation", "Joy", "Trust",
-                  "Sadness", "Fear", "Surprise", "Disgust"];
   const myKey = "Ab7efgCTM4wUS3UD0wzdSdgIYDyj8H0q";
-
   const query = "http://api.giphy.com/v1/gifs/search?q="
-                + topics[0] + "&api_key=" + myKey + "&limit=10";
+                + pickedTopic + "&api_key=" + myKey + "&limit=10";
+  let topicDiv;
+  let isExpanded = true;
+
+  generateList();
+
+  function generateList() {
+    for(let i = 0; i < topics.length; i++) {
+      topicDiv = $("<div class='topic'>");
+      topicDiv.html("<p>" + topics[i] + "</p>");
+      $("#content").append(topicDiv);
+      topicDiv.hide();
+    }
+    let tracker = 1;
+    const cascade = setInterval(function() {
+      $(".topic:nth-of-type(" + tracker + ")").fadeIn(1000);
+      tracker++;
+      if(tracker === topics.length + 1) {
+        clearInterval(cascade);
+      }
+    }, 50);
+  }
+
+  $(document).on("click", ".topic", function() {
+    $(".container").toggleClass("containerExpand");
+    $(this).toggleClass("expanded");
+  });
+
   $.ajax({
     url: query,
     method: "GET"
